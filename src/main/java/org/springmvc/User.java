bd73c604dcb1f62cc3528884405ee474e119448e
Marko Niemi <marko.j.niemi@gmail.com>
2016-01-18 12:28:54 +0200

    Added MySQL config.

--- src/main/java/org/springmvc/User.java
+++ src/main/java/org/springmvc/User.java
@@ -1,5 +1,8 @@
 package org.springmvc;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.xml.bind.annotation.XmlRootElement;
 
 import lombok.Data;
@@ -9,9 +12,11 @@ import lombok.Data;
  * @Data annotation from Lombok adds getters and setter to User.
  * @XmlRootElement is required for REST/XML.
  */
+@Entity
 @Data
 @XmlRootElement
 public class User {
+    @GeneratedValue @Id
     private Long id;
     private String name;
     private String email;
